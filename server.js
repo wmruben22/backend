e6e96382a147c7cd6675a8b8b7ba6634d83bf25c
wmruben22 <116590587+wmruben22@users.noreply.github.com>
2022-11-23 15:46:25 -0500

    Finish Module 5 Lab

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/messages', (req, res) => {
+
+    const messages =  ["yes", "each", "messages"];
+
+    res.send(messages);
+});
+
+app.listen(port, () => console.log('app running'));
